ffc06366326c13deef24c4f2058f8b4b6e3f4763
Christoffer Dall <christoffer.dall@linaro.org>
2015-02-25 14:21:16 +0100

    NOT_FOR_UPSTREAM: Assign a no-op hypercall for measurements

--- arch/arm/kvm/handle_exit.c
+++ arch/arm/kvm/handle_exit.c
@@ -43,6 +43,19 @@ static int handle_hvc(struct kvm_vcpu *vcpu, struct kvm_run *run)
 	trace_kvm_hvc(*vcpu_pc(vcpu), *vcpu_reg(vcpu, 0),
 		      kvm_vcpu_hvc_get_imm(vcpu));
 
+	/*
+	 * Enable cycle counter for Xen - we choose to be compatible but rely
+	 * on running measurement guests under perf on the KVM host.
+	 */
+	if (*vcpu_reg(vcpu, 0) == 0x4b000001) {
+		return 1;
+	}
+
+	/* NOOP hvc call to measure hypercall turn-around time */
+	if (*vcpu_reg(vcpu, 0) == 0x4b000000) {
+		return 1;
+	}
+
 	ret = kvm_psci_call(vcpu);
 	if (ret < 0) {
 		kvm_inject_undefined(vcpu);
