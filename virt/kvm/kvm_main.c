4a25c03d15bffcf7d842b09979c630afbfa747d8
Shih-Wei Li <shihwei@cs.columbia.edu>
2015-04-22 08:54:58 -0600

    Fixed error in previous commit

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2027,9 +2027,7 @@ void kvm_vcpu_block(struct kvm_vcpu *vcpu)
 			break;
 
 		waited = true;
-		kvm_arch_sched_out(vcpu);
 		schedule();
-		kvm_arch_sched_in(vcpu, 1);
 	}
 
 	finish_wait(&vcpu->wq, &wait);
@@ -3503,7 +3501,7 @@ static void kvm_sched_in(struct preempt_notifier *pn, int cpu)
 	if (vcpu->preempted)
 		vcpu->preempted = false;
 
-	//kvm_arch_sched_in(vcpu, cpu);
+	kvm_arch_sched_in(vcpu, cpu);
 
 	kvm_arch_vcpu_load(vcpu, cpu);
 }
@@ -3516,7 +3514,7 @@ static void kvm_sched_out(struct preempt_notifier *pn,
 	if (current->state == TASK_RUNNING)
 		vcpu->preempted = true;
 
-	//kvm_arch_sched_out(vcpu);
+	kvm_arch_sched_out(vcpu);
 
 	kvm_arch_vcpu_put(vcpu);
 }
