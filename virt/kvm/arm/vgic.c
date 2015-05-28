6f5916226afe6e3a7f7e7bb630b612aabfad19d9
Christoffer Dall <christoffer.dall@linaro.org>
2015-05-28 16:32:33 +0200

    Revert "HACK: arm/arm64: KVM: Do not offset the irq by VGIC_NR_PRIVATE_IRQS"
    This reverts commit 619c57c335bc0a3a355f44d571aa077c8bfe35f6.
    
    With the proper QEMU IRQ mapping fixed, this is no longer needed.
    
    Necessary code in the columbia-armvirt-v2.3.0 branch of QEMU.

--- virt/kvm/arm/vgic.c
+++ virt/kvm/arm/vgic.c
@@ -2171,13 +2171,15 @@ int kvm_irq_map_chip_pin(struct kvm *kvm, unsigned irqchip, unsigned pin)
 int kvm_set_irq(struct kvm *kvm, int irq_source_id,
 		u32 irq, int level, bool line_status)
 {
+	unsigned int spi = irq + VGIC_NR_PRIVATE_IRQS;
+
 	trace_kvm_set_irq(irq, level, irq_source_id);
 
 	BUG_ON(!vgic_initialized(kvm));
 
-	if (irq > kvm->arch.vgic.nr_irqs)
+	if (spi > kvm->arch.vgic.nr_irqs)
 		return -EINVAL;
-	return kvm_vgic_inject_irq(kvm, 0, irq, level);
+	return kvm_vgic_inject_irq(kvm, 0, spi, level);
 
 }
 
